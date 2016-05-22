9ecb154d0022b12d31ad701ba2ea0de06b27a3a5
Jelle Sjollema <djsjollema@gmail.com>
2016-05-22 10:55:47 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# opdracht tilesheet
+
+Haal tiles uit een spritesheet en maak er een (dynamisch) speelveld van
---
+++ script.js
@@ -0,0 +1,6 @@
+window.addEventListener("load",function(){
+	var canvas = document.getElementById("canvas");
+	var context = canvas.getContext("2d");
+
+
+});
